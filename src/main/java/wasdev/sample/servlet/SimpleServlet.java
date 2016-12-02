a3a14e165d9b8a3ecde95e32b6ff637b9ff6c496
thuykn <thuykn@us.ibm.com>
2016-12-02 14:49:58 -0500

    Update SimpleServlet.java

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -1,12 +1,20 @@
 package wasdev.sample.servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//add new import
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -21,7 +29,35 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        //response.getWriter().print("Hello World!");
+
+        // New code  
+               Context initContext;
+		try {
+			initContext = new InitialContext();
+			DataSource ds = (DataSource)initContext.lookup("jdbc/pet-finder-dashDB for Analytics-4o");
+			Connection conn = ds.getConnection();
+		    //System.out.println("yes");
+			response.getWriter().print("DashDB is connected now!</br>");
+			response.getWriter().print("</br>Dogs table: NAME, AGE, DOGTYPE, COST</br>");
+		    Statement stmt = conn.createStatement();
+          
+			
+			stmt.execute("DELETE FROM DOGS WHERE ID = 7");
+		    stmt.execute("INSERT INTO DOGS VALUES (7, 'NEW', 6, 'NEW', 100.00 )");
+            stmt.executeQuery("SELECT * FROM DOGS");
+
+		    ResultSet rs = stmt.getResultSet();
+			System.out.println("yes");
+		    while(rs.next()) {
+                 response.getWriter().print(rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5)+"</br>");
+			} 
+			stmt.close();
+			conn.close();
+		    } catch (NamingException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }

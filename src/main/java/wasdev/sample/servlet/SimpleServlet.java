328971a581a0d06f0dfcd3a0925d00403547a219
thuykn <thuykn@us.ibm.com>
2016-12-02 15:32:30 -0500

    Update SimpleServlet.java

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -42,20 +42,14 @@ public class SimpleServlet extends HttpServlet {
 			response.getWriter().print("</br>Dogs table:</br> NAME, AGE, DOGTYPE, COST</br>");
 		    	Statement stmt = conn.createStatement();
        
-			response.getWriter().print("After Dogs table!</br>");
 			stmt.execute("DELETE FROM DOGS WHERE ID = 7");
-			response.getWriter().print("After delete row 7!</br>");
 		    	stmt.execute("INSERT INTO DOGS VALUES (7, 'NEW', 6, 'NEW', 100.00 )");
-			response.getWriter().print("After insert NEW1!</br>");
             		stmt.executeQuery("SELECT * FROM DOGS");
-			response.getWriter().print("After Select statement!</br>");
 
 		    	ResultSet rs = stmt.getResultSet();
 			System.out.println("yes");
 		    	while(rs.next()) {
-				response.getWriter().print("Inside While loop before writing dog data !</br>");
                  		response.getWriter().print(rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5)+"</br>");
-				response.getWriter().print("Inside While loop after writing dog data !</br>");
 			} 
 			stmt.close();
 			conn.close();
